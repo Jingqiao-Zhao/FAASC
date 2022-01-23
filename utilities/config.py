8bf1eda5e8d288b6682feb8d6e30a21f28646cb8
Jingqiao-Zhao <67094127+Jingqiao-Zhao@users.noreply.github.com>
2022-01-23 17:02:04 +0800

    Update config.py

--- utilities/config.py
+++ utilities/config.py
@@ -6,15 +6,7 @@ fmin = 0  # Hz
 fmax = 24000  # Hz
 num_channel = 2
 
-'''
-sample_rate = 44100
-window_size = 2048
-hop_size = 1024    # So that there are 64 frames per second
-mel_bins = 128
-fmin = 0      # Hz
-fmax  = 22050   # Hz
-num_channel = 2
-'''
+
 frames_per_second = sample_rate // hop_size
 audio_duration = 10  # Audio recordings in DCASE2019 Task1 are all 10 seconds
 frames_num = frames_per_second * audio_duration
@@ -26,24 +18,34 @@ labels = ['airport', 'shopping_mall', 'metro_station', 'street_pedestrian',
 lb_to_idx = {lb: idx for idx, lb in enumerate(labels)}
 idx_to_lb = {idx: lb for idx, lb in enumerate(labels)}
 
-dataset_dir = '/home/zjq/data/'
-workspace = '/home/zjq/data/'
+Data_years='2020'
 
+if Data_years=='2019':
+    dataset_dir = '/home/zjq/data/'
+    workspace = '/home/zjq/data/'
+    sub_dir = 'TAU-urban-acoustic-scenes-2019-mobile-development'
+    sources_to_train = ['a', 'b', 'c']
+    sources_to_evaluate = ['a', 'b', 'c']
+    evaluate_iteration = 6400
+    evaluate_accs = 0.5571
 
-# dataset_dir = '/home/zjq/data/2020task1a'
-# workspace = '/home/zjq/data/2020task1a'
-sub_dir = 'TAU-urban-acoustic-scenes-2020-mobile-development'
-#sub_dir = 'TAU-urban-acoustic-scenes-2019-mobile-development'
+elif Data_years=='2020':
+    dataset_dir = '/home/zjq/data/'
+    workspace = '/home/zjq/data/'
+    sub_dir = 'TAU-urban-acoustic-scenes-2020-mobile-development'
+    sources_to_train = ['a', 'b', 'c', 's1', 's2', 's3']
+    sources_to_evaluate = ['a', 'b', 'c', 's1', 's2', 's3', 's4', 's5', 's6']
+    evaluate_iteration = 8800
+    evaluate_accs = 0.7221
+
+# Set this option to True if you want to see feature maps as they are aligned
+feature_maps=False
+
 model_type = 'FeatureS'
 batch_size = 64
 cuda = True
+cuda_id='0'
 loss='KL_WODA'
-# evaluate_iteration = 8800
-evaluate_iteration = 6400
-# evaluate_accs = 0.7221
-evaluate_accs = 0.5571
 epoch = 100
-sources_to_train = ['a', 'b', 'c', 's1', 's2', 's3']
-sources_to_evaluate = ['a', 'b', 'c', 's1', 's2', 's3', 's4', 's5', 's6']
-# sources_to_train = ['a', 'b', 'c']
-# sources_to_evaluate = ['a', 'b', 'c']
+
+
